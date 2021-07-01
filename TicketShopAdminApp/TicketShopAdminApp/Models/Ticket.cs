96bf21ad9fc9860c814b6a3463de01a80faa93cf
Viktorija <viktorija.nikolovska@hotmail.com>
2021-07-01 23:39:29 +0200

    Admin app added

---
+++ TicketShopAdminApp/TicketShopAdminApp/Models/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketShopAdminApp.Models
+{
+    public class Ticket
+    {
+
+        public string TicketName { get; set; }
+
+        public string TicketImage { get; set; }
+
+        public string TicketDescription { get; set; }
+
+        public int TicketPrice { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
